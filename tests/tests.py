48f5ed7d7419f675da49b1a7a4e2d6a0c2c8b9a3
alex <tukipechenka@gmail.com>
2024-06-24 09:59:08 +1000

    Changes

---
+++ tests/tests.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+def test_status_code_recipes():
+    response = client.get('/recipes/')
+    assert response.status_code == 200
