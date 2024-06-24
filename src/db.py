dadee4034b34ff7764cc3106bc966ebd1e1549fb
alex <tukipechenka@gmail.com>
2024-06-24 11:31:54 +1000

    Update #5

--- src/db.py
+++ src/db.py
@@ -1,16 +1,15 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext import asyncio
 from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.py.db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = asyncio.create_async_engine(DATABASE_URL, echo=True)
 # expire_on_commit=False will prevent attributes from being expired
 # after commit.
-async_session = async_sessionmaker(
+async_session = asyncio.async_sessionmaker(
     engine,
     expire_on_commit=False,
-    class_=AsyncSession,
+    class_=asyncio.AsyncSession,
 )
 session = async_session()
 Base = declarative_base()
